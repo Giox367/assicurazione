12d6fd79df65f95086a9929ceca55525f2a4d48a
Giox367 <111007060+Giox367@users.noreply.github.com>
2023-10-24 16:41:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ phoneButton.addEventListener('mouseover', () => {
 phoneButton.addEventListener('mouseout', () => {
     phoneButton.textContent = '+1 123-456-7890';
 });
+document.getElementById('phone-button').addEventListener('click', function() {
+    var phoneNumber = '+11234567890'; // Sostituisci con il tuo numero di telefono
+    window.location.href = 'tel:' + phoneNumber;
+});
